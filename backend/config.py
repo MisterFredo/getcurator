11d4aacb668f287c38b43807190ff08f1f5a9d35
MisterFredo <mister.fredo@gmail.com>
2026-07-11 22:13:28 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,19 +1,39 @@
 # backend/config.py
+
 import os
 
 # ---------------------------------------------------------
 # BigQuery configuration
 # ---------------------------------------------------------
-# Les deux variables doivent être définies dans Render :
-# - GOOGLE_CREDENTIALS_FILE -> /etc/secrets/bq.json
-# - BQ_PROJECT -> "adex-5555"
-# - BQ_DATASET -> "RATECARD"
+# Les variables sont définies dans Render.
+#
+# DEV
+#   BQ_PROJECT = getcurator
+#   BQ_DATASET = GETCURATOR_DEV
+#
+# PROD
+#   BQ_PROJECT = getcurator
+#   BQ_DATASET = GETCURATOR_PROD
+#
+# Secret :
+#   GOOGLE_CREDENTIALS_FILE = /etc/secrets/bq.json
+# ---------------------------------------------------------
 
-BQ_PROJECT = os.getenv("BQ_PROJECT", "adex-5555")
-BQ_DATASET = os.getenv("BQ_DATASET", "RATECARD")
+BQ_PROJECT = os.getenv(
+    "BQ_PROJECT",
+    "getcurator",
+)
+
+BQ_DATASET = os.getenv(
+    "BQ_DATASET",
+    "GETCURATOR_PROD",
+)
 
 # ---------------------------------------------------------
-# Mode environnement
+# Environment
 # ---------------------------------------------------------
-ENV = os.getenv("ENV", "local")
 
+ENV = os.getenv(
+    "ENV",
+    "local",
+)
