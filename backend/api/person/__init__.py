7f869c2dc6f12cf3fbc5c41e8543d828086fdd75
MisterFredo <mister.fredo@gmail.com>
2026-07-11 12:53:53 +0200

    Delete backend/api/person directory

--- backend/api/person/__init__.py
+++
@@ -1,7 +0,0 @@
-# backend/api/person/__init__.py
-
-from fastapi import APIRouter
-from api.person.routes import router as person_routes
-
-router = APIRouter()
-router.include_router(person_routes)
