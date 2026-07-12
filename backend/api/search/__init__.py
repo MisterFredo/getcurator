f84edf1cb43dfb39996fdac5b762a081a74bd48c
MisterFredo <mister.fredo@gmail.com>
2026-07-12 16:38:49 +0200

    Delete backend/api/search directory

--- backend/api/search/__init__.py
+++
@@ -1,7 +0,0 @@
-# backend/api/search/__init__.py
-
-from fastapi import APIRouter
-from api.search.routes import router as search_routes
-
-router = APIRouter()
-router.include_router(search_routes)
