50b4d35a26879a089e0f63408884e3b2a6c9df32
MisterFredo <mister.fredo@gmail.com>
2026-07-12 16:17:38 +0200

    Delete backend/api/synthesis directory

--- backend/api/synthesis/__init__.py
+++
@@ -1,7 +0,0 @@
-# backend/api/synthesis/__init__.py
-
-from fastapi import APIRouter
-from api.synthesis.routes import router as synthesis_routes
-
-router = APIRouter()
-router.include_router(synthesis_routes)
