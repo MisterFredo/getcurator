31f148e6050e468feb5bdb5642bc8df2b793841c
MisterFredo <mister.fredo@gmail.com>
2026-07-13 14:05:21 +0200

    Create content.ts

---
+++ frontend/types/content.ts
@@ -0,0 +1,39 @@
+export type ContentFilters = {
+  search: string;
+
+  company_id: string;
+
+  solution_id: string;
+
+  topic_id: string;
+
+  concept_id: string;
+
+  source_id: string;
+
+  date_from: string;
+
+  date_to: string;
+
+  only_numbers: boolean;
+};
+
+export const EMPTY_CONTENT_FILTERS: ContentFilters = {
+  search: "",
+
+  company_id: "",
+
+  solution_id: "",
+
+  topic_id: "",
+
+  concept_id: "",
+
+  source_id: "",
+
+  date_from: "",
+
+  date_to: "",
+
+  only_numbers: false,
+};
