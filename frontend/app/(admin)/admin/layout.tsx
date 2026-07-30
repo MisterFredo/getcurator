b8564fa3c2ff65e8436a98c19987cb1ab28ecf9e
MisterFredo <mister.fredo@gmail.com>
2026-07-30 21:27:55 +0200

    Update layout.tsx

--- frontend/app/(admin)/admin/layout.tsx
+++ frontend/app/(admin)/admin/layout.tsx
@@ -1,9 +1,25 @@
 import AdminShell from "./AdminShell";
 
+import {
+  DrawerProvider,
+} from "@/contexts/DrawerContext";
+
+import DrawerHost from "@/components/drawers/DrawerHost";
+
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AdminShell>{children}</AdminShell>;
+  return (
+    <DrawerProvider>
+
+      <AdminShell>
+        {children}
+      </AdminShell>
+
+      <DrawerHost />
+
+    </DrawerProvider>
+  );
 }
