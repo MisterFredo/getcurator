fd29505c389be8a1a17efab11a53cf74bffd0e5b
MisterFredo <mister.fredo@gmail.com>
2026-07-31 15:11:27 +0200

    Create html_footer.py

---
+++ backend/core/digest/html_footer.py
@@ -0,0 +1,23 @@
+# backend/core/digest/html_footer.py
+
+
+# ============================================================
+# FOOTER
+# ============================================================
+
+def render_footer() -> str:
+    """
+    Render the email footer.
+    """
+
+    return """
+<tr>
+
+<td class="footer">
+
+Powered by GetCurator
+
+</td>
+
+</tr>
+"""
