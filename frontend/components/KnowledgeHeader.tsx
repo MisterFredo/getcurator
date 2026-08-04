82558f5e74ac9594a7a4ac6a28082bb50b5734e9
MisterFredo <mister.fredo@gmail.com>
2026-08-04 15:45:26 +0200

    Create KnowledgeHeader.tsx

---
+++ frontend/components/KnowledgeHeader.tsx
@@ -0,0 +1,23 @@
+export default function KnowledgeHeader() {
+
+  return (
+
+    <div>
+
+      <h1 className="text-3xl font-semibold text-ratecard-blue">
+
+        Knowledge
+
+      </h1>
+
+      <p className="mt-1 text-gray-500">
+
+        Build, monitor and manage GetCurator Experts.
+
+      </p>
+
+    </div>
+
+  );
+
+}
