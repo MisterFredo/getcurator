91b8ecf9d7e7ecc1aa82ea6726e85627c5472b2c
MisterFredo <mister.fredo@gmail.com>
2026-08-04 15:49:26 +0200

    Update KnowledgeDashboard.tsx

--- frontend/components/knowledge/KnowledgeDashboard.tsx
+++ frontend/components/knowledge/KnowledgeDashboard.tsx
@@ -1,10 +1,58 @@
+import KnowledgeStatCard from "./KnowledgeStatCard";
+
 export default function KnowledgeDashboard() {
 
   return (
 
-    <div className="rounded-lg border bg-white p-6">
+    <div className="grid grid-cols-2 gap-4 lg:grid-cols-6">
 
-      Dashboard
+      <KnowledgeStatCard
+
+        title="Companies"
+
+        value={0}
+
+      />
+
+      <KnowledgeStatCard
+
+        title="Topics"
+
+        value={0}
+
+      />
+
+      <KnowledgeStatCard
+
+        title="Solutions"
+
+        value={0}
+
+      />
+
+      <KnowledgeStatCard
+
+        title="Knowledge"
+
+        value={0}
+
+      />
+
+      <KnowledgeStatCard
+
+        title="Users"
+
+        value={0}
+
+      />
+
+      <KnowledgeStatCard
+
+        title="Experts"
+
+        value={0}
+
+      />
 
     </div>
 
