5e6e0377cbddb981ee137aa99932169605949fce
MisterFredo <mister.fredo@gmail.com>
2026-08-04 15:56:34 +0200

    Update DrawerHost.tsx

--- frontend/components/drawers/DrawerHost.tsx
+++ frontend/components/drawers/DrawerHost.tsx
@@ -1,37 +1,97 @@
+// frontend/components/drawers/DrawerHost.tsx
+
 "use client";
 
 import { useDrawer } from "@/contexts/DrawerContext";
 
 import AnalysisDrawerAdmin from "@/components/drawers/AnalysisDrawerAdmin";
 import DigestPreviewDrawer from "@/components/drawers/DigestPreviewDrawer";
+import KnowledgeDrawer from "@/components/knowledge/KnowledgeDrawer";
 
 /* ========================================================= */
 
 export default function DrawerHost() {
 
   const {
+
     rightDrawer,
+
     closeRightDrawer,
+
   } = useDrawer();
 
   return (
-    <>
-      {rightDrawer.type === "analysis" &&
-        rightDrawer.id && (
-          <AnalysisDrawerAdmin
-            contentId={rightDrawer.id}
-            onClose={closeRightDrawer}
-          />
-      )}
 
-      {rightDrawer.type === "digest-preview" &&
+    <>
+
+      {/* =================================================== */}
+      {/* ANALYSIS */}
+      {/* =================================================== */}
+
+      {
+
+        rightDrawer.type === "analysis" &&
         rightDrawer.id && (
-          <DigestPreviewDrawer
-            digestId={rightDrawer.id}
+
+          <AnalysisDrawerAdmin
+
+            contentId={rightDrawer.id}
+
             onClose={closeRightDrawer}
+
           />
-      )}
+
+        )
+
+      }
+
+      {/* =================================================== */}
+      {/* DIGEST */}
+      {/* =================================================== */}
+
+      {
+
+        rightDrawer.type === "digest-preview" &&
+        rightDrawer.id && (
+
+          <DigestPreviewDrawer
+
+            digestId={rightDrawer.id}
+
+            onClose={closeRightDrawer}
+
+          />
+
+        )
+
+      }
+
+      {/* =================================================== */}
+      {/* KNOWLEDGE */}
+      {/* =================================================== */}
+
+      {
+
+        rightDrawer.type === "knowledge" &&
+        rightDrawer.id &&
+        rightDrawer.entityType && (
+
+          <KnowledgeDrawer
+
+            entityId={rightDrawer.id}
+
+            entityType={rightDrawer.entityType}
+
+            onClose={closeRightDrawer}
+
+          />
+
+        )
+
+      }
+
     </>
+
   );
 
 }
