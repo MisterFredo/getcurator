89e9757163aa3904746ec902025d22c0971693cb
MisterFredo <mister.fredo@gmail.com>
2026-08-04 15:47:25 +0200

    Create KnowledgeExplorer.tsx

---
+++ frontend/components/knowledge/KnowledgeExplorer.tsx
@@ -0,0 +1,25 @@
+type Props = {
+
+  onOpen: (
+    entity: any,
+  ) => void;
+
+};
+
+export default function KnowledgeExplorer({
+
+  onOpen,
+
+}: Props) {
+
+  return (
+
+    <div className="rounded-lg border bg-white p-6">
+
+      Explorer
+
+    </div>
+
+  );
+
+}
