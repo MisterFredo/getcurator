f8eb8dc79fe15e474f2048256c01c24169817d07
MisterFredo <mister.fredo@gmail.com>
2026-08-06 23:49:38 +0200

    Update routes.py

--- backend/api/search/routes.py
+++ backend/api/search/routes.py
@@ -1,1 +1,13 @@
 
+@router.post("/search")
+def search_route(
+    request: ContentSearchRequest
+):
+    try:
+        return search_contents(request)
+
+    except Exception as e:
+        raise HTTPException(
+            400,
+            str(e)
+        )
