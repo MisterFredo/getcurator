2e11d7f40e70c68f97cb69af7c85170012b174cf
MisterFredo <mister.fredo@gmail.com>
2026-08-06 23:18:21 +0200

    Update models.py

--- backend/api/common/models.py
+++ backend/api/common/models.py
@@ -1,3 +1,7 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional, List, Literal
+from datetime import datetime, date
+
 class BulkIdsRequest(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
