29d19c9a169b9167fd5902b52bfa776f24d24fff
MisterFredo <mister.fredo@gmail.com>
2026-08-06 16:11:40 +0200

    Update admin_service.py

--- backend/core/content/admin_service.py
+++ backend/core/content/admin_service.py
@@ -1,1 +1,33 @@
+# ============================================================
+# IMPORTS
+# ============================================================
+
+from config import (
+    BQ_PROJECT,
+    BQ_DATASET,
+)
+
+from utils.bigquery_utils import (
+    query_bq,
+)
+
+# ============================================================
+# TABLES
+# ============================================================
+
+TABLE_CONTENT = (
+    f"{BQ_PROJECT}.{BQ_DATASET}.RATECARD_CONTENT"
+)
+
+TABLE_COMPANY = (
+    f"{BQ_PROJECT}.{BQ_DATASET}.RATECARD_COMPANY"
+)
+
+TABLE_SOURCE = (
+    f"{BQ_PROJECT}.{BQ_DATASET}.RATECARD_SOURCE"
+)
+
+TABLE_CONTENT_RAW = (
+    f"{BQ_PROJECT}.{BQ_DATASET}.RATECARD_CONTENT_RAW"
+)
 
