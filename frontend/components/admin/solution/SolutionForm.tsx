184c7e2162b586872f75a27883c851f761220923
MisterFredo <mister.fredo@gmail.com>
2026-08-13 22:52:23 +0200

    Update SolutionForm.tsx

--- frontend/components/admin/solution/SolutionForm.tsx
+++ frontend/components/admin/solution/SolutionForm.tsx
@@ -122,35 +122,46 @@ export default function SolutionForm({
     if (!solutionId) {
       return;
     }
-
-    const s =
+  
+    const res =
       await api.get(
         `/solution/${solutionId}`
       );
-
+  
+    const s =
+      res.solution;
+  
+    if (!s) {
+  
+      throw new Error(
+        "Solution not found."
+      );
+  
+    }
+  
     setSolution({
-
+  
       name:
         s.name ?? "",
-
+  
       id_company:
         s.id_company ?? "",
-
+  
       description:
         s.description ?? "",
-
+  
       content:
         s.content ?? "",
-
+  
       aliases:
         s.aliases ?? [],
-
+  
     });
-
+  
     setLogoFilename(
       s.media_logo_rectangle_id ?? null
     );
-
+  
   }
 
   /* =======================================================
