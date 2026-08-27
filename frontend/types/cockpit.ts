daf78dd42bee5d6a7686395b9b0d9f249858ba35
MisterFredo <mister.fredo@gmail.com>
2026-08-28 01:48:47 +0200

    Update cockpit.ts

--- frontend/types/cockpit.ts
+++ frontend/types/cockpit.ts
@@ -3,6 +3,7 @@
 ========================================================= */
 
 export type DestockMonitoring = {
+
   run_time: string;
 
   total: number;
@@ -16,42 +17,70 @@ export type DestockMonitoring = {
   errors: number;
 
   progress_pct: number;
+
 };
+
 
 /* ========================================================= */
 
 export type TranslationMonitoring = {
+
   total_contents: number;
 
-  title_en_done: number;
+  english_source_ready: number;
 
-  excerpt_en_done: number;
+  english_source_missing: number;
+
+  title_fr_done: number;
+
+  excerpt_fr_done: number;
+
+  content_body_fr_done: number;
+
+  signal_fr_done: number;
+
+  mecanique_fr_done: number;
+
+  enjeu_fr_done: number;
+
+  friction_fr_done: number;
 
   fully_translated: number;
 
   missing_translation: number;
 
   pct_fully_translated: number;
+
 };
+
 
 /* ========================================================= */
 
 export type CockpitMonitoring = {
+
   destock: DestockMonitoring;
 
   translation: TranslationMonitoring;
+
 };
+
 
 /* =========================================================
    QUALITY
 ========================================================= */
 
 export type QualityRow = {
-  [key: string]: unknown;
+
+  [key: string]:
+    unknown;
+
 };
 
+
 export type QualityResponse = {
+
   status: string;
 
   results: QualityRow[];
+
 };
