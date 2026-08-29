81d3f99c082d83d8a359b6bb7ca786f0dd1b4d89
MisterFredo <mister.fredo@gmail.com>
2026-08-29 20:00:16 +0200

    Update AdminDigestList.tsx

--- frontend/components/digest/AdminDigestList.tsx
+++ frontend/components/digest/AdminDigestList.tsx
@@ -266,7 +266,7 @@ export default function AdminDigestList({
                 digest,
               ) => (
 
-                <DigestRow
+                <AdminDigestRow
 
                   key={
                     digest.id
