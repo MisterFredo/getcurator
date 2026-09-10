8cf5258c0491c64db30dd0f8ff19ff3c05ddcd09
MisterFredo <mister.fredo@gmail.com>
2026-09-10 23:04:28 +0200

    Update page.tsx

--- frontend-curator/app/(core)/numbers/page.tsx
+++ frontend-curator/app/(core)/numbers/page.tsx
@@ -645,10 +645,12 @@ export default function NumbersPage() {
             className="
               grid
               grid-cols-1
-              gap-4
+              gap-3
               sm:grid-cols-2
-              lg:grid-cols-3
-              xl:grid-cols-4
+              md:grid-cols-3
+              lg:grid-cols-4
+              xl:grid-cols-5
+              2xl:grid-cols-6
             "
           >
 
