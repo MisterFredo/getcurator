4e25d33523a4f0e6ed74868f5998399f0be1c7bd
MisterFredo <mister.fredo@gmail.com>
2026-09-21 11:16:56 +0200

    Create route.ts

---
+++ frontend-curator/app/feed/route.ts
@@ -0,0 +1,35 @@
+import {
+  NextRequest,
+  NextResponse,
+} from "next/server";
+
+export function GET(
+  request: NextRequest,
+) {
+
+  const target =
+    new URL(
+      "/",
+      request.url,
+    );
+
+  request.nextUrl.searchParams.forEach(
+    (
+      value,
+      key,
+    ) => {
+
+      target.searchParams.set(
+        key,
+        value,
+      );
+
+    },
+  );
+
+  return NextResponse.redirect(
+    target,
+    307,
+  );
+
+}
